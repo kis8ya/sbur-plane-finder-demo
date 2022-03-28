c44e51bd12df7d99e28d47f4306a12a7d206b057
kis8ya <kis8P@yandex.ru>
2022-03-29 00:41:59 +0300

    added a base page for aircraft

---
+++ src/main/java/com/github/kis8ya/sburplanefinderdemo/AircraftRepository.java
@@ -0,0 +1,5 @@
+package com.github.kis8ya.sburplanefinderdemo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AircraftRepository extends CrudRepository<Aircraft, Long> {}
