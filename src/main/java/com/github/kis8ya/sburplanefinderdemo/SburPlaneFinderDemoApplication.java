90b03603f85e7f539969a9e2c963f908584178fd
kis8ya <kis8P@yandex.ru>
2022-03-29 00:06:17 +0300

    initial commit

---
+++ src/main/java/com/github/kis8ya/sburplanefinderdemo/SburPlaneFinderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.kis8ya.sburplanefinderdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburPlaneFinderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SburPlaneFinderDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kis8ya/sburplanefinderdemo/SburPlaneFinderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kis8ya.sburplanefinderdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SburPlaneFinderDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
